52fa8e0db81722b14e4d74728c0f36af5aba38b8
Michael Ababio <michael.ababio@johnsonfit.com>
2019-07-16 13:07:30 -0500

    - Set up Babel; Modified notes

--- 3 - Hello React/public/scripts/app.js
+++ 3 - Hello React/public/scripts/app.js
@@ -1,8 +1,13 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX - Javascript XML
-// var template = <p>This is JSX from app.js!</p>
-var template = React.createElement("p", null, "This is JSX from app.js!");
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
